c5e1b4b1200a69b1dd1c1d545128ed0cc0863ba4
Nguyen Hoang Nam <nammon72@gmail.com>
2019-04-30 17:40:00 +0700

    Input and Print

---
+++ basic.py
@@ -0,0 +1,7 @@
+name = input('What is your name?\n')
+year_birth = input('What is your year birthday?\n')
+your_old = 2019 - year_birth
+favorite_color = input('What is your favorite color?\n')
+print('Hi, '+name)
+print('You are '+your_old)
+print('Your favorite color is '+favorite_color)
