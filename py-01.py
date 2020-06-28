f52817df114b76495f13ff08742893ef1dfdf426
Nguyen Hoang Nam <nammon72@Nguyens-MacBook-Pro.local>
2020-06-28 16:16:17 +0700

    update py-01.py

--- py-01.py
+++ py-01.py
@@ -1,12 +1,5 @@
-from datetime import datetime
-from datetime import date
-from datetime import time
-print("----- hello, python! -----")
-name = input("What your name?\n")
-age = int(input("How old are you?\n")
-today = date.today()
-year = today.strftime("%Y")
-
-print("Hello,",year)
-
-
+def main():
+	print("Hello World!")
+if __name__ == "__main__":
+	main()
+print("Guru99")
